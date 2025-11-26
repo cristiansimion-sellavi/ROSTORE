160199c0912e18f17470d56cf266d6f80c953638
cristiansimion-sellavi <cristian_simion@sellavi.com>
2025-11-26 14:02:14 +0200

    Delete 100381index.js

--- 100381index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // dacă body nu are încă clasa, i-o adăugăm
-  if (!document.body.classList.contains("shuffle-theme")) {
-    document.body.classList.add("shuffle-theme");
-  }
-});
