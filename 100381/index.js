bc4cb4e8bc95c8b5b2fe341d66606df91140d8cb
cristiansimion-sellavi <cristian_simion@sellavi.com>
2025-11-26 14:04:35 +0200

    Create index.js

---
+++ 100381/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // dacă body nu are încă clasa, i-o adăugăm
+  if (!document.body.classList.contains("shuffle-theme")) {
+    document.body.classList.add("shuffle-theme");
+  }
+});
